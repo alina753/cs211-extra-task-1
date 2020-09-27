2decdc6053a359c294d970120d1bcd6459248741
alina753 <ramazanovaalina905@gmail.com>
2020-09-27 21:34:38 +0300

    last task done and tests passed

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -37,84 +37,23 @@ int get_seconds(int sec)
 {
     return sec % 60;
 }
-/*
-    Implement three functions
-        * get_hours
-        * get_minutes
-        * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
-    of a time in seconds. E.g.:
-
-    >>> get_hours(3800)
-    1
-
-    >>> get_minutes(3800)
-    3
-
-    >>> get_seconds(3800)
-    20
-
-    In other words, if 3800 seconds have elapsed since midnight, 
-    it is currently 01:03:20 (hh:mm:ss).
-*/
 
 double time_to_utc(int utc_offset, double time)
 {
-    /*
-        Return time at UTC+0, where utc_offset is the number of hours away from
-        UTC+0.
-        You may be interested in:
-        https://en.wikipedia.org/wiki/Coordinated_Universal_Time
-
-        >>> time_to_utc(+0, 12.0)
-        12.0
- 
-        >>> time_to_utc(+1, 12.0)
-        11.0
- 
-        >>> time_to_utc(-1, 12.0)
-        13.0
- 
-        >>> time_to_utc(-11, 18.0)
-        5.0
- 
-        >>> time_to_utc(-1, 0.0)
-        1.0
- 
-        >>> time_to_utc(-1, 23.0)
-        0.0
-    */
+    assert(time >= 0);
+    double c = time - utc_offset;
+    return (c >= 24) ? c - 24 : c;
 }
 
 double time_from_utc(int utc_offset, double time)
 {
-    /*
-        Return UTC time in time zone utc_offset.
-
-        >>> time_from_utc(+0, 12.0)
-        12.0
- 
-        >>> time_from_utc(+1, 12.0)
-        13.0
- 
-        >>> time_from_utc(-1, 12.0)
-        11.0
- 
-        >>> time_from_utc(+6, 6.0)
-        12.0
- 
-        >>> time_from_utc(-7, 6.0)
-        23.0
- 
-        >>> time_from_utc(-1, 0.0)
-        23.0
- 
-        >>> time_from_utc(-1, 23.0)
-        22.0
- 
-        >>> time_from_utc(+1, 23.0)
-        0.0
-    */
+    assert(time >= 0);
+    double c = time + utc_offset;
+    if (c < 0)
+        return 24 + c;
+    if (c >= 24)
+        return   c - 24;
+    else return c;
 }
 
 int main()
@@ -142,4 +81,20 @@ int main()
     assert(get_hours(3800) == 1);
     assert(get_minutes(3800) == 3);
     assert(get_seconds(3800) == 20);
+
+    assert(time_to_utc(+0, 12.0) == 12.0);
+    assert(time_to_utc(+1, 12.0) == 11.0);
+    assert(time_to_utc(-1, 12.0) == 13.0);
+    assert(time_to_utc(-11, 18.0) == 5.0);
+    assert(time_to_utc(-1, 0.0) == 1.0);
+    assert(time_to_utc(-1, 23.0) == 0.0);
+
+    assert(time_from_utc(+0, 12.0) == 12.0);
+    assert(time_from_utc(+1, 12.0) == 13.0);
+    assert(time_from_utc(-1, 12.0) == 11.0);
+    assert(time_from_utc(+6, 6.0) == 12.0);
+    assert(time_from_utc(-7, 6.0) == 23.0);
+    assert(time_from_utc(-1, 0.0) == 23.0);
+    assert(time_from_utc(-1, 23.0) == 22.0);
+    assert(time_from_utc(+1, 23.0) == 0.0);
 }
